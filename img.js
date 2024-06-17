e4fc71cc552f72933d220afbba34fee2364ecbf4
2024chokokadaiHTML <m.emushigoto@gmail.com>
2024-06-17 13:58:49 +0900

    Update img.js

--- img.js
+++ img.js
@@ -1,13 +1,13 @@
 function changeImage(imageNumber) {
   var imagePath;
   if (imageNumber === 1) {
-    imagePath = "https://github.com/2024chokokadaiHTML/chokomap2024.github.io/blob/main/floormap_1f.svg";
+    imagePath = "floormap_1f.svg";
   } else if (imageNumber === 2) {
-    imagePath = "https://github.com/2024chokokadaiHTML/chokomap2024.github.io/blob/main/floormap_1f.svg";
+    imagePath = "floormap_1f.svg";
   } else if (imageNumber === 3) {
-    imagePath = "https://github.com/2024chokokadaiHTML/chokomap2024.github.io/blob/main/floormap_1f.svg";
+    imagePath = "floormap_1f.svg";
   }else if (imageNumber === 4) {
-    imagePath = "https://github.com/2024chokokadaiHTML/chokomap2024.github.io/blob/main/floormap_1f.svg";
+    imagePath = "floormap_1f.svg";
   }
   document.getElementById("image").src = imagePath;
 }
