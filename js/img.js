970e32d73ed6ddf1b12cd91144421e77a1eea628
2024chokokadaiHTML <m.emushigoto@gmail.com>
2024-08-26 15:13:35 +0900

    Update img.js

--- js/img.js
+++ js/img.js
@@ -1,13 +1,13 @@
 function changeImage(imageNumber) {
   var imagePath;
   if (imageNumber === 1) {
-    imagePath = "floormap_1f.svg";
+    imagePath = "js/floormap_1f.svg";
   } else if (imageNumber === 2) {
-    imagePath = "floormap_2f.svg";
+    imagePath = "js/floormap_2f.svg";
   } else if (imageNumber === 3) {
-    imagePath = "floormap_3f.svg";
+    imagePath = "js/floormap_3f.svg";
   }else if (imageNumber === 4) {
-    imagePath = "floormap_4f.svg";
+    imagePath = "js/floormap_4f.svg";
   }
   document.getElementById("image").src = imagePath;
 }
